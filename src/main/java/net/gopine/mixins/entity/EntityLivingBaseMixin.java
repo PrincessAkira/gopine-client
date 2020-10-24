9c4f6231814d4354b425b321ce5d595d80892cd7
Yukii <broxmodz@gmail.com>
2020-10-24 17:52:33 +0200

    -- Removes annoying Hurtcam
    If needed add settings yourself im too dumb for that

---
+++ src/main/java/net/gopine/mixins/entity/EntityLivingBaseMixin.java
@@ -0,0 +1,43 @@
+package net.gopine.mixins.entity;
+import net.gopine.events.impl.player.EventAttackEntity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+/**
+ * EntityPlayer mixin class used to add events.
+ * @author Yukii | Azariel#0004
+ * @since b0.1
+ */
+
+@Mixin(EntityLivingBase.class)
+public class EntityLivingBaseMixin {
+
+  @Shadow
+  private int maxHurtTime;
+  @Shadow
+  private int hurtTime;
+  @Shadow
+  private float attackedAtYaw;
+
+  /**
+   * Disables Hurtcam
+   * @param ci unused
+   * @author Yukii | Azariel#0004
+   * @since b0.1
+   */
+
+  @Inject(method = "performHurtAnimation", at = @At("RETURN"))
+  public void performHurtAnimation(CallbackInfo ci)
+  {
+    this.hurtTime = this.maxHurtTime = 0;
+    this.attackedAtYaw = 0.0F;
+  }
+
+
+}
